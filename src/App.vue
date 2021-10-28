e905b5903d3ee45793e2d700d1c007a0f25c1075
Kevin <sxzz@sxzz.moe>
2021-10-28 14:54:33 +0800

    chore: upgrade dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <el-button>Test Button</el-button>
 
-  <el-alert show-icon>
-    <template #title>Test Button</template>
-  </el-alert>
+  <div>
+    <el-alert show-icon>
+      <template #title>Test Button</template>
+    </el-alert>
+  </div>
+
+  <el-row>
+    <el-col>
+      <div v-loading="true">Hello</div>
+    </el-col>
+  </el-row>
 </template>
